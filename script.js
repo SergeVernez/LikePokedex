e61d3b3d1b60ba30349d77f8ee05c21e8253873f
SergeVernez <sergevernez.pro@gmail.com>
2024-12-12 10:10:17 +0100

    Ajout JS

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () =>{
+    const pokemonContainer = document.getElementById('pokemonCard');
+
+    function fetchPokemon() {
+        fetch("https://pokeapi.co/api/v2/pokemon")
+    }
+})
