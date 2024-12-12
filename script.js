84d846045e0b56e2d077daaa5e56ee168c202daf
SergeVernez <sergevernez.pro@gmail.com>
2024-12-12 10:33:47 +0100

    ajout console.log

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
 	//--- Appel de l'API pokémon---
 	function fetchPokemon() {
-		fetch("https://pokeapi.co/api/v2/pokemon");
+		fetch("https://pokeapi.co/api/v2/pokemon")
+			.then((response) => response.json())
+			.then((data) => {
+				console.log(data);
+			});
 	}
 });
