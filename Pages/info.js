4a7c1537886a6633dd83f9860ffb0d00971ee385
SergeVernez <sergevernez.pro@gmail.com>
2024-12-19 10:22:02 +0100

    modif js

--- Pages/info.js
+++ Pages/info.js
@@ -1,3 +1,4 @@
-const info = () =>{
-    
-}
+// --- On charge tout le document (DOM) pour s'assurer que le script s'applique à tout le DOM--
+document.addEventListener("DOMContentLoaded", () => {
+    const pokemonInfo = document.getElementById("infoCard");
+});
