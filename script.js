41b0e854251f48a0e82f6fd0453997a079fbfa52
SergeVernez <sergevernez.pro@gmail.com>
2025-01-10 16:26:10 +0100

    modif commentaires

--- script.js
+++ script.js
@@ -175,8 +175,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
 	// Retourne en haut de la page lorsque l'utilisateur clique sur le bouton
 	document.getElementById("backToTop").addEventListener("click", function () {
-		document.body.scrollTop = 0; // Pour Safari
-		document.documentElement.scrollTop = 0; // Pour Chrome, Firefox, IE et Opera
+		document.body.scrollTop = 0; // Pour Apple
+		document.documentElement.scrollTop = 0; // Pour Chrome, etc...
 	});
 });
 
